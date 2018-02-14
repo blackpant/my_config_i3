01a2f97fd6fe0bf4b5df8587b7a3680d6ce576d2
Blackpant <saadi_4@hotmail.fr>
2018-02-14 13:24:16 +0100

    suppression de .bashrc + update install.py et Notes

--- install.py
+++ install.py
@@ -6,28 +6,39 @@ import sys
 import os.path as p
 import glob
 
-if "HOME" in os.environ:
-    userPath = os.getenv('HOME')
-    configPath = userPath + "/.config"
+def CheckFolder() :
+    #
+    if "HOME" in os.environ:
+        userPath = os.getenv('HOME')
+        configPath = userPath + "/.config"
 
-    if ".config" in os.listdir(userPath):
-        folderConfFile = ["i3","rofi","compton"]
-        for conf in folderConfFile:
-            if conf in os.listdir(configPath):
-                print("OK")
-            else:
-                print("Création du dossier %s",conf)
-                print("###################################################")
-                try:
-                    subprocess.call(["mkdir", configPath + "/" + conf])
-                except subprocess.CalledProcessError as error:
-                    print("Erreur : \r")
-                    sys.exit(error.returncode)
+        if ".config" in os.listdir(userPath):
+            folderConfFile = ["i3","rofi","compton"]
+            for conf in folderConfFile:
+                if conf in os.listdir(configPath):
+                    print("OK")
+                else:
+                    print("Création du dossier %s",conf)
+                    print("###################################################")
+                    try:
+                        subprocess.call(["mkdir", configPath + "/" + conf])
+                    except subprocess.CalledProcessError as error:
+                        print("Erreur : \r")
+                        sys.exit(error.returncode)
 
-    else :
-        print("NO")
+        else :
+            print("NO")
 
 
-    print(userPath)
-    userConfigFolder = os.listdir(configPath)
-    print(userConfigFolder)
+def CheckFolderBis():
+    # change current directory
+    # get environnement variable $HOME
+    # copy .config from repositery to user home folder
+    # install antigen + dependencies
+    # copy .zshrc from repositery to $HOME
+    # install vim ultimate config
+    # copy from repositerie vim_runtime/source_non_forked/ to ~/.vim_runtine/
+    subprocess.check_call()
+
+def Main():
+    pass
